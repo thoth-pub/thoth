1e384fec5156fe67d8dea76540bf7a972b0e92c2
Javier Arias <javier@arias.re>
2020-01-29 14:31:04 +0000

    upgrade actix

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,6 @@ use std::sync::Arc;
 
 use actix_web::{web, App, Error, HttpResponse, HttpServer};
 use dotenv::dotenv;
-use futures::future::Future;
 use juniper::http::graphiql::graphiql_source;
 use juniper::http::GraphQLRequest;
 
@@ -22,31 +21,30 @@ pub mod sql_types;
 use crate::db::establish_connection;
 use crate::graphql_schema::{create_schema, Context, Schema};
 
-fn graphiql() -> HttpResponse {
+async fn graphiql() -> HttpResponse {
     let html = graphiql_source("http://localhost:8080/graphql");
     HttpResponse::Ok()
         .content_type("text/html; charset=utf-8")
         .body(html)
 }
 
-fn graphql(
+async fn graphql(
     st: web::Data<Arc<Schema>>,
     ctx: web::Data<Context>,
     data: web::Json<GraphQLRequest>,
-) -> impl Future<Item = HttpResponse, Error = Error> {
-    web::block(move || {
+) -> Result<HttpResponse, Error> {
+    let result = web::block(move || {
         let res = data.execute(&st, &ctx);
         Ok::<_, serde_json::error::Error>(serde_json::to_string(&res)?)
     })
-    .map_err(Error::from)
-    .and_then(|user| {
-        Ok(HttpResponse::Ok()
-            .content_type("application/json")
-            .body(user))
-    })
+    .await?;
+    Ok(HttpResponse::Ok()
+        .content_type("application/json")
+        .body(result))
 }
 
-fn main() -> io::Result<()> {
+#[actix_rt::main]
+async fn main() -> io::Result<()> {
      dotenv().ok();
     let pool = establish_connection();
     let schema_context = Context { db: pool.clone() };
@@ -55,9 +53,10 @@ fn main() -> io::Result<()> {
         App::new()
             .data(schema.clone())
             .data(schema_context.clone())
-            .service(web::resource("/graphql").route(web::post().to_async(graphql)))
+            .service(web::resource("/graphql").route(web::post().to(graphql)))
             .service(web::resource("/graphiql").route(web::get().to(graphiql)))
     })
     .bind("localhost:8080")?
     .run()
+    .await
 }
