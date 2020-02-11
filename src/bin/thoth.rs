0109c649fe5c304a937f45fa3eeff42dbbb475d3
Javier Arias <javier@arias.re>
2020-02-11 15:44:37 +0000

    abstract server logic

--- src/bin/thoth.rs
+++ src/bin/thoth.rs
@@ -1,24 +1,14 @@
 use std::io;
 
-use actix_web::{web, App, HttpServer};
-use dotenv::dotenv;
+use actix_web::{App, HttpServer};
 
-use thoth::server::{graphql, graphiql};
-use thoth::db::establish_connection;
-use thoth::graphql_handlers::{create_schema, Context};
+use thoth::server::config;
 
 #[actix_rt::main]
 async fn main() -> io::Result<()> {
-    dotenv().ok();
-    let pool = establish_connection();
-    let schema_context = Context { db: pool.clone() };
-    let schema = std::sync::Arc::new(create_schema());
     HttpServer::new(move || {
         App::new()
-            .data(schema.clone())
-            .data(schema_context.clone())
-            .service(web::resource("/graphql").route(web::post().to(graphql)))
-            .service(web::resource("/graphiql").route(web::get().to(graphiql)))
+            .configure(config)
     })
     .bind("localhost:8080")?
     .run()
--- src/server.rs
+++ src/server.rs
@@ -1,10 +1,12 @@
 use std::sync::Arc;
 
 use actix_web::{web, Error, HttpResponse};
+use dotenv::dotenv;
 use juniper::http::graphiql::graphiql_source;
 use juniper::http::GraphQLRequest;
 
-use crate::graphql_handlers::{Context, Schema};
+use crate::db::establish_connection;
+use crate::graphql_handlers::{create_schema, Context, Schema};
 
 pub async fn graphiql() -> HttpResponse {
     let html = graphiql_source("/graphql");
@@ -26,4 +28,24 @@ pub async fn graphql(
     Ok(HttpResponse::Ok()
         .content_type("application/json")
         .body(result))
+}
+
+pub fn config(cfg: &mut web::ServiceConfig) {
+    dotenv().ok();
+    let pool = establish_connection();
+    let schema_context = Context { db: pool.clone() };
+    let schema = std::sync::Arc::new(create_schema());
+
+    cfg.data(
+        schema.clone()
+    );
+    cfg.data(
+        schema_context.clone()
+    );
+    cfg.service(
+        web::resource("/graphql").route(web::post().to(graphql))
+    );
+    cfg.service(
+        web::resource("/graphiql").route(web::get().to(graphiql))
+    );
 }
