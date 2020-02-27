23a167038f1c549801972a49923101637ca7a0b0
Javier Arias <javier@arias.re>
2020-02-27 13:33:05 +0000

    Create a single binary with subcommands
    closes #6

--- src/bin/thoth.rs
+++ src/bin/thoth.rs
@@ -1,16 +1,63 @@
+#[macro_use]
+extern crate diesel_migrations;
+extern crate clap;
 use std::io;
+use std::io::stdout;
 
-use actix_web::{App, HttpServer};
+use clap::{Arg, App, AppSettings};
+use actix_web::{App as WebApp, HttpServer};
+use diesel_migrations::embed_migrations;
 
 use thoth::server::config;
+use thoth::db::establish_connection;
 
 #[actix_rt::main]
-async fn main() -> io::Result<()> {
+async fn start_server(port: String) -> io::Result<()> {
     HttpServer::new(move || {
-        App::new()
+        WebApp::new()
             .configure(config)
     })
-    .bind("0.0.0.0:8080")?
+    .bind(format!("0.0.0.0:{}", port))?
     .run()
     .await
+}
+
+fn run_migrations() -> io::Result<()> {
+    embed_migrations!("migrations");
+    let connection = establish_connection().get().unwrap();
+    embedded_migrations::run_with_output(&connection, &mut stdout())
+        .expect("Can't run migrations");
+    Ok(())
+}
+
+fn main() -> io::Result<()> {
+    let matches = App::new("Thoth")
+        .version("0.1.0")
+        .author("Javier Arias <javi@openbookpublishers.com>")
+        .about("GraphQL API for bibliographic data")
+        .setting(AppSettings::SubcommandRequiredElseHelp)
+        .subcommand(
+            App::new("migrate")
+                .about("Run the database migrations"))
+        .subcommand(
+            App::new("start")
+                .about("Start the thoth server")
+                .arg(Arg::with_name("port")
+                    .short("p")
+                    .long("port")
+                    .value_name("PORT")
+                    .help("Port to bind")
+                    .takes_value(true)))
+        .get_matches();
+
+    match matches.subcommand() {
+            ("start", Some(start_matches)) => {
+                let port = start_matches.value_of("port").unwrap();
+                start_server(port.to_owned())
+            }
+            ("migrate", Some(_)) => {
+                run_migrations()
+            }
+            _ => unreachable!(),
+    }
 }
