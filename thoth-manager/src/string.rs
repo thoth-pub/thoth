6a1b9f137b497ab0d578e3c31031a32e7fcacbcd
Javier Arias <javier@arias.re>
2020-09-16 12:08:23 +0100

    Remove unused constants

--- thoth-manager/src/string.rs
+++ thoth-manager/src/string.rs
@@ -6,10 +6,4 @@ macro_rules! strings {
 
 strings! {
     GRAPHQL_ENDPOINT => "http://localhost:8000/graphql",
-    AUTHENTICATION_ERROR => "Authentication failed",
-    INPUT_PASSWORD => "Password",
-    INPUT_EMAIL => "Email",
-    REQUEST_ERROR => "Failed to send request to server",
-    RESPONSE_ERROR => "Failed to retrieve valid server response",
-    TEXT_LOGIN => "Login",
 }
