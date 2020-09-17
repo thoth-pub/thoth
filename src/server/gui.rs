acb44e2b47c9b49418d9fff7a4897ea189e39c93
Javier Arias <javier@arias.re>
2020-09-17 16:00:36 +0100

    Serve wasm statically from thoth binary

--- src/server/gui.rs
+++ src/server/gui.rs
@@ -7,75 +7,42 @@ use actix_web::HttpServer;
 use actix_web::web;
 use dotenv::dotenv;
 
+macro_rules! static_files {
+    ($(($cname:ident, $fname:ident) => ($source_path:expr, $dest_path:expr, $type:expr),)*) => (
+        $(
+            const $cname: &[u8] = include_bytes!($source_path);
+
+            #[get($dest_path)]
+            async fn $fname() -> HttpResponse {
+                HttpResponse::Ok().content_type($type).body($cname)
+            }
+        )*
+
+        fn config(cfg: &mut web::ServiceConfig) {
+            dotenv().ok();
+            $(cfg.service($fname);)*
+        }
+
+    )
+}
+
+static_files! {
+    (MATOMO, matomo_file) => ("../static/js/matomo.js", "/js/matomo.js", "application/javascript"),
+    (JS, js_file) => ("../static/main.js", "/main.js", "application/javascript"),
+    (PKG, pkg_file) => ("../static/pkg/thoth_manager.js", "/pkg/thoth_manager.js", "application/javascript"),
+    (WASM, wasm_file) => ("../static/pkg/thoth_manager_bg.wasm", "/pkg/thoth_manager_bg.wasm", "application/wasm"),
+    (BULMA, bulma_file) => ("../static/css/bulma-pageloader.min.css", "/css/bulma-pageloader.min.css", "text/css; charset=utf-8"),
+    (CSS, css_file) => ("../static/css/thoth.css", "/css/thoth.css", "text/css; charset=utf-8"),
+    (ICON, icon_file) => ("../static/favicon.ico", "/favicon.ico", "image/x-icon"),
+    (LOGO, logo_file) => ("../static/img/thoth-logo.png", "/img/thoth-logo.png", "image/png"),
+}
+
 const INDEX_FILE: &[u8] = include_bytes!("../static/index.html");
-const MATOMO_FILE: &[u8] = include_bytes!("../static/js/matomo.js");
-const JS_FILE: &[u8] = include_bytes!("../static/main.js");
-const PKG_FILE: &[u8] = include_bytes!("../static/pkg/thoth_manager.js");
-const WASM_FILE: &[u8] = include_bytes!("../static/pkg/thoth_manager_bg.wasm");
-const BULMA_FILE: &[u8] = include_bytes!("../static/css/bulma-pageloader.min.css");
-const CSS_FILE: &[u8] = include_bytes!("../static/css/thoth.css");
-const ICON_FILE: &[u8] = include_bytes!("../static/favicon.ico");
-const LOGO_FILE: &[u8] = include_bytes!("../static/img/thoth-logo.png");
-
-#[get("/pkg/thoth_manager_bg.wasm")]
-async fn wasm_file() -> HttpResponse {
-    HttpResponse::Ok().content_type("application/wasm").body(WASM_FILE)
-}
-
-#[get("/main.js")]
-async fn js_file() -> HttpResponse {
-    HttpResponse::Ok().content_type("application/javascript").body(JS_FILE)
-}
-
-#[get("/js/matomo.js")]
-async fn matomo_file() -> HttpResponse {
-    HttpResponse::Ok().content_type("application/javascript").body(MATOMO_FILE)
-}
-
-#[get("/pkg/thoth_manager.js")]
-async fn pkg_file() -> HttpResponse {
-    HttpResponse::Ok().content_type("application/javascript").body(PKG_FILE)
-}
-
-#[get("/css/thoth.css")]
-async fn css_file() -> HttpResponse {
-    HttpResponse::Ok().content_type("text/css; charset=utf-8").body(CSS_FILE)
-}
-
-#[get("/css/bulma-pageloader.min.css")]
-async fn bulma_file() -> HttpResponse {
-    HttpResponse::Ok().content_type("text/css; charset=utf-8").body(BULMA_FILE)
-}
-
-#[get("/favicon.ico")]
-async fn favicon() -> HttpResponse {
-    HttpResponse::Ok()
-        .content_type("image/x-icon")
-        .body(ICON_FILE)
-}
-
-#[get("/img/thoth-logo.png")]
-async fn logo() -> HttpResponse {
-    HttpResponse::Ok().content_type("image/png").body(LOGO_FILE)
-}
-
 
 async fn index() -> HttpResponse {
     HttpResponse::Ok()
         .content_type("text/html; charset=utf-8")
         .body(INDEX_FILE)
-}
-
-fn config(cfg: &mut web::ServiceConfig) {
-    dotenv().ok();
-    cfg.service(wasm_file);
-    cfg.service(js_file);
-    cfg.service(matomo_file);
-    cfg.service(pkg_file);
-    cfg.service(css_file);
-    cfg.service(bulma_file);
-    cfg.service(favicon);
-    cfg.service(logo);
 }
 
 #[actix_rt::main]
