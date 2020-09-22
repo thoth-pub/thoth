ed4eb0d6c7de27370b0e6c425bbdeecb2d61845b
Javier Arias <javier@arias.re>
2020-09-22 11:16:51 +0100

    Implement user friendly text for work status and work type

--- thoth-api/src/models/work.rs
+++ thoth-api/src/models/work.rs
@@ -1,3 +1,4 @@
+use std::fmt;
 use chrono::naive::NaiveDate;
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
@@ -115,4 +116,37 @@ pub struct NewWork {
     pub toc: Option<String>,
     pub cover_url: Option<String>,
     pub cover_caption: Option<String>,
+}
+
+impl fmt::Display for WorkType {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            WorkType::BookChapter => write!(f, "Book Chapter"),
+            WorkType::Monograph => write!(f, "Monograph"),
+            WorkType::EditedBook => write!(f, "Edited Book"),
+            WorkType::Textbook => write!(f, "Textbook"),
+            WorkType::JournalIssue => write!(f, "Journal Issue"),
+            WorkType::BookSet => write!(f, "Book Set"),
+        }
+    }
+}
+
+impl fmt::Display for WorkStatus {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            WorkStatus::Unspecified => write!(f, "Unspecified"),
+            WorkStatus::Cancelled => write!(f, "Cancelled"),
+            WorkStatus::Forthcoming => write!(f, "Forthcoming"),
+            WorkStatus::PostponedIndefinitely => write!(f, "Postponed Indefinitely"),
+            WorkStatus::Active => write!(f, "Active"),
+            WorkStatus::NoLongerOurProduct => write!(f, "No Longer Our Product"),
+            WorkStatus::OutOfStockIndefinitely => write!(f, "Out Of Stock Indefinitely"),
+            WorkStatus::OutOfPrint => write!(f, "Out Of Print"),
+            WorkStatus::Inactive => write!(f, "Inactive"),
+            WorkStatus::Unknown => write!(f, "Unknown"),
+            WorkStatus::Remaindered => write!(f, "Remaindered"),
+            WorkStatus::WithdrawnFromSale => write!(f, "Withdrawn From Sale"),
+            WorkStatus::Recalled => write!(f, "Recalled"),
+        }
+    }
 }
