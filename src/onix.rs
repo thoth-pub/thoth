a623ffe35d6e9e1b2a2cb7453309c72780c7336d
Javier Arias <javier@arias.re>
2020-09-23 15:22:31 +0100

    Use iterators

--- src/onix.rs
+++ src/onix.rs
@@ -332,8 +332,7 @@ fn handle_event<W: Write>(w: &mut EventWriter<W>, work: &mut WorkQueryWork) -> R
                     .ok();
                 })
                 .ok();
-                let mut sequence_number = 0;
-                for contribution in &work.contributions {
+                for (mut sequence_number, contribution) in work.contributions.iter().enumerate() {
                     sequence_number += 1;
                     write_element_block("Contributor", None, None, w, |w| {
                         write_element_block("SequenceNumber", None, None, w, |w| {
