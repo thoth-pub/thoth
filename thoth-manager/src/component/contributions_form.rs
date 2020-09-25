344b952ed62b598f1effb1f4b39cb7ed1cdbda02
Javier Arias <javier@arias.re>
2020-09-25 16:18:23 +0100

    Filter contributors on seach component

--- thoth-manager/src/component/contributions_form.rs
+++ thoth-manager/src/component/contributions_form.rs
@@ -1,12 +1,17 @@
 use yew::html;
 use yew::prelude::*;
 use yew::ComponentLink;
+use yewtil::fetch::Fetch;
 use yewtil::fetch::FetchAction;
 use yewtil::fetch::FetchState;
 use yewtil::future::LinkFuture;
 
+use crate::api::contributors_query::CONTRIBUTORS_QUERY;
+use crate::api::contributors_query::ContributorsRequest;
+use crate::api::contributors_query::ContributorsRequestBody;
 use crate::api::contributors_query::FetchActionContributors;
 use crate::api::contributors_query::FetchContributors;
+use crate::api::contributors_query::Variables;
 use crate::api::contribution_types_query::FetchActionContributionTypes;
 use crate::api::contribution_types_query::FetchContributionTypes;
 use crate::api::models::Contribution;
@@ -18,6 +23,7 @@ use crate::component::utils::FormContributionTypeSelect;
 pub struct ContributionsFormComponent {
     contributions: Vec<Contribution>,
     data: ContributionsFormData,
+    show_results: bool,
     fetch_contributors: FetchContributors,
     fetch_contribution_types: FetchContributionTypes,
     link: ComponentLink<Self>,
@@ -33,6 +39,8 @@ pub enum Msg {
     GetContributors,
     SetContributionTypesFetchState(FetchActionContributionTypes),
     GetContributionTypes,
+    ToggleSearchResultDisplay(bool),
+    SearchContributor(String),
 }
 
 #[derive(Clone, Properties)]
@@ -50,6 +58,7 @@ impl Component for ContributionsFormComponent {
             contributors: vec![],
             contribution_types: vec![],
         };
+        let show_results = false;
 
         link.send_message(Msg::GetContributors);
         link.send_message(Msg::GetContributionTypes);
@@ -57,6 +66,7 @@ impl Component for ContributionsFormComponent {
         ContributionsFormComponent {
             contributions,
             data,
+            show_results,
             fetch_contributors: Default::default(),
             fetch_contribution_types: Default::default(),
             link,
@@ -99,6 +109,23 @@ impl Component for ContributionsFormComponent {
                     .send_message(Msg::SetContributionTypesFetchState(FetchAction::Fetching));
                 false
             }
+            Msg::ToggleSearchResultDisplay(value) => {
+                self.show_results = value;
+                true
+            }
+            Msg::SearchContributor(value) => {
+                let body = ContributorsRequestBody {
+                    query: CONTRIBUTORS_QUERY.to_string(),
+                    variables: Variables {
+                        work_id: None,
+                        filter: Some(value),
+                    },
+                };
+                let request = ContributorsRequest { body };
+                self.fetch_contributors = Fetch::new(request);
+                self.link.send_message(Msg::GetContributors);
+                false
+            }
         }
     }
 
@@ -113,12 +140,36 @@ impl Component for ContributionsFormComponent {
                     { "Contributions" }
                 </p>
                 <div class="panel-block">
-                    <p class="control has-icons-left">
-                    <input class="input" type="text" placeholder="Search Contributor" />
-                    <span class="icon is-left">
-                        <i class="fas fa-search" aria-hidden="true"></i>
-                    </span>
-                    </p>
+                    <div class=self.search_dropdown_status() style="width: 100%">
+                        <div class="dropdown-trigger" style="width: 100%">
+                            <div class="field">
+                                <p class="control is-expanded has-icons-left">
+                                    <input
+                                        class="input"
+                                        type="search"
+                                        placeholder="Search Contributor"
+                                        aria-haspopup="true"
+                                        aria-controls="contributors-menu"
+                                        oninput=self.link.callback(|e: InputData| Msg::SearchContributor(e.value))
+                                        onfocus=self.link.callback(|_| Msg::ToggleSearchResultDisplay(true))
+                                        onblur=self.link.callback(|_| Msg::ToggleSearchResultDisplay(false))
+                                    />
+                                    <span class="icon is-left">
+                                        <i class="fas fa-search" aria-hidden="true"></i>
+                                    </span>
+                                </p>
+                            </div>
+                        </div>
+                        <div class="dropdown-menu" id="contributors-menu" role="menu">
+                            <div class="dropdown-content">
+                                {
+                                    for self.data.contributors.iter().map(|c| html!{
+                                        <a href="#" class="dropdown-item">{ &c.full_name }</a>
+                                    })
+                                }
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 { for self.contributions.iter().map(|c| self.render_contribution(c)) }
             </nav>
@@ -127,6 +178,13 @@ impl Component for ContributionsFormComponent {
 }
 
 impl ContributionsFormComponent {
+    fn search_dropdown_status(&self) -> String {
+        match self.show_results {
+            true => "dropdown is-active".to_string(),
+            false => "dropdown".to_string(),
+        }
+    }
+
     fn render_contribution(&self, c: &Contribution) -> Html {
         html! {
             <div class="panel-block field is-horizontal">
