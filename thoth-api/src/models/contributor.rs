6ddd4e647d22d9704f596a13f7dd161d408d8405
Javier Arias <javier@arias.re>
2020-10-05 15:24:21 +0100

    Implement tests for string to contribution type

--- thoth-api/src/models/contributor.rs
+++ thoth-api/src/models/contributor.rs
@@ -1,11 +1,13 @@
 use serde::{Deserialize, Serialize};
 use std::fmt;
+use std::str::FromStr;
 use uuid::Uuid;
 
 #[cfg(feature = "backend")]
 use crate::schema::contribution;
 #[cfg(feature = "backend")]
 use crate::schema::contributor;
+use crate::errors::ThothError;
 
 #[cfg_attr(feature = "backend", derive(DbEnum, juniper::GraphQLEnum))]
 #[cfg_attr(feature = "backend", DieselType = "Contribution_type")]
@@ -87,6 +89,26 @@ impl fmt::Display for ContributionType {
     }
 }
 
+impl FromStr for ContributionType {
+    type Err = ThothError;
+
+    fn from_str(input: &str) -> Result<ContributionType, ThothError> {
+        match input {
+            "Author" => Ok(ContributionType::Author),
+            "Editor" => Ok(ContributionType::Editor),
+            "Translator" => Ok(ContributionType::Translator),
+            "Photographer" => Ok(ContributionType::Photographer),
+            "Ilustrator" => Ok(ContributionType::Ilustrator),
+            "Music Editor" => Ok(ContributionType::MusicEditor),
+            "Foreword By" => Ok(ContributionType::ForewordBy),
+            "Introduction By" => Ok(ContributionType::IntroductionBy),
+            "Afterword By" => Ok(ContributionType::AfterwordBy),
+            "Preface By" => Ok(ContributionType::PrefaceBy),
+            _  => Err(ThothError::InvalidContributionType(input.to_string())),
+        }
+    }
+}
+
 #[test]
 fn test_contributiontype_display() {
     assert_eq!(format!("{}", ContributionType::Author), "Author");
@@ -105,4 +127,21 @@ fn test_contributiontype_display() {
     );
     assert_eq!(format!("{}", ContributionType::AfterwordBy), "Afterword By");
     assert_eq!(format!("{}", ContributionType::PrefaceBy), "Preface By");
+}
+
+#[test]
+fn test_contributiontype_fromstr() {
+    assert_eq!(ContributionType::from_str("Author").unwrap(), ContributionType::Author);
+    assert_eq!(ContributionType::from_str("Editor").unwrap(), ContributionType::Editor);
+    assert_eq!(ContributionType::from_str("Translator").unwrap(), ContributionType::Translator);
+    assert_eq!(ContributionType::from_str("Photographer").unwrap(), ContributionType::Photographer);
+    assert_eq!(ContributionType::from_str("Ilustrator").unwrap(), ContributionType::Ilustrator);
+    assert_eq!(ContributionType::from_str("Music Editor").unwrap(), ContributionType::MusicEditor);
+    assert_eq!(ContributionType::from_str("Foreword By").unwrap(), ContributionType::ForewordBy);
+    assert_eq!(ContributionType::from_str("Introduction By").unwrap(), ContributionType::IntroductionBy);
+    assert_eq!(ContributionType::from_str("Afterword By").unwrap(), ContributionType::AfterwordBy);
+    assert_eq!(ContributionType::from_str("Preface By").unwrap(), ContributionType::PrefaceBy);
+
+    assert!(ContributionType::from_str("Juggler").is_err());
+    assert!(ContributionType::from_str("Supervisor").is_err());
 }
