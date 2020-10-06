e491d061b79757fb24e4fcf6c59a4111380bba6b
Javier Arias <javier@arias.re>
2020-10-06 14:20:31 +0100

    Start using string constants

--- thoth-manager/src/string.rs
+++ thoth-manager/src/string.rs
@@ -8,4 +8,6 @@ strings! {
     INPUT_EMAIL => "Email",
     INPUT_PASSWORD => "Password",
     TEXT_LOGIN => "Login",
+    EMPTY_CONTRIBUTIONS => "This work does not have any contributions. Search contributors above to add their contributions.",
+    EMPTY_ISSUES => "This work is not part of a series. Search above to add a new series issue.",
 }
