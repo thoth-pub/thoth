3c5318fb60b82af1ca9de57792a12f202c507210
Javier Arias <javier@arias.re>
2020-10-06 18:08:42 +0100

    Implement from/to string conversion for publication type

--- thoth-api/src/errors.rs
+++ thoth-api/src/errors.rs
@@ -12,6 +12,8 @@ pub enum ThothError {
     InvalidWorkStatus(String),
     #[fail(display = "{} is not a valid Contribution Type", _0)]
     InvalidContributionType(String),
+    #[fail(display = "{} is not a valid Publication Type", _0)]
+    InvalidPublicationType(String),
     #[fail(display = "{} is not a valid Series Type", _0)]
     InvalidSeriesType(String),
     #[fail(display = "Database error: {}", _0)]
--- thoth-api/src/models/publication.rs
+++ thoth-api/src/models/publication.rs
@@ -1,6 +1,9 @@
 use serde::{Deserialize, Serialize};
+use std::fmt;
+use std::str::FromStr;
 use uuid::Uuid;
 
+use crate::errors::ThothError;
 #[cfg(feature = "backend")]
 use crate::schema::publication;
 
@@ -40,4 +43,60 @@ pub struct NewPublication {
     pub work_id: Uuid,
     pub isbn: Option<String>,
     pub publication_url: Option<String>,
+}
+
+impl fmt::Display for PublicationType {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            PublicationType::Paperback => write!(f, "Paperback"),
+            PublicationType::Hardback => write!(f, "Hardback"),
+            PublicationType::PDF => write!(f, "PDF"),
+            PublicationType::HTML => write!(f, "HTML"),
+            PublicationType::XML => write!(f, "XML"),
+            PublicationType::Epub => write!(f, "Epub"),
+            PublicationType::Mobi => write!(f, "Mobi"),
+        }
+    }
+}
+
+impl FromStr for PublicationType {
+    type Err = ThothError;
+
+    fn from_str(input: &str) -> Result<PublicationType, ThothError> {
+        match input {
+            "Paperback" => Ok(PublicationType::Paperback),
+            "Hardback" => Ok(PublicationType::Hardback),
+            "PDF" => Ok(PublicationType::PDF),
+            "HTML" => Ok(PublicationType::HTML),
+            "XML" => Ok(PublicationType::XML),
+            "Epub" => Ok(PublicationType::Epub),
+            "Mobi" => Ok(PublicationType::Mobi),
+            _ => Err(ThothError::InvalidPublicationType(input.to_string())),
+        }
+    }
+}
+
+#[test]
+fn test_publicationtype_display() {
+    assert_eq!(format!("{}", PublicationType::Paperback), "Paperback");
+    assert_eq!(format!("{}", PublicationType::Hardback), "Hardback");
+    assert_eq!(format!("{}", PublicationType::PDF), "PDF");
+    assert_eq!(format!("{}", PublicationType::HTML), "HTML");
+    assert_eq!(format!("{}", PublicationType::XML), "XML");
+    assert_eq!(format!("{}", PublicationType::Epub), "Epub");
+    assert_eq!(format!("{}", PublicationType::Mobi), "Mobi");
+}
+
+#[test]
+fn test_publicationtype_fromstr() {
+    assert_eq!(PublicationType::from_str("Paperback").unwrap(), PublicationType::Paperback);
+    assert_eq!(PublicationType::from_str("Hardback").unwrap(), PublicationType::Hardback);
+    assert_eq!(PublicationType::from_str("PDF").unwrap(), PublicationType::PDF);
+    assert_eq!(PublicationType::from_str("HTML").unwrap(), PublicationType::HTML);
+    assert_eq!(PublicationType::from_str("XML").unwrap(), PublicationType::XML);
+    assert_eq!(PublicationType::from_str("Epub").unwrap(), PublicationType::Epub);
+    assert_eq!(PublicationType::from_str("Mobi").unwrap(), PublicationType::Mobi);
+
+    assert!(PublicationType::from_str("PNG").is_err());
+    assert!(PublicationType::from_str("Latex").is_err());
 }
