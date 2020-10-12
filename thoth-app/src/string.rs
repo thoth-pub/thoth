69a343d223bd752be4623aa24a65d8aa2a933ab7
Javier Arias <javier@arias.re>
2020-10-12 16:20:02 +0100

    Add string literals

--- thoth-app/src/string.rs
+++ thoth-app/src/string.rs
@@ -5,6 +5,8 @@ macro_rules! strings {
 }
 
 strings! {
+    YES => "Yes",
+    NO => "No",
     INPUT_EMAIL => "Email",
     INPUT_PASSWORD => "Password",
     TEXT_LOGIN => "Login",
@@ -16,6 +18,7 @@ strings! {
     PAGINATION_COUNT_WORKS => "Displaying works",
     EMPTY_CONTRIBUTIONS => "This work does not have any contributions. Search contributors above to add its contributions.",
     EMPTY_ISSUES => "This work is not part of a series. Search above to add a new series issue.",
+    EMPTY_LANGUAGES => "This work does not have any languages. Search above to add a new language.",
     EMPTY_PUBLICATIONS => "This work does not have any publications. Click above to add associated publications",
     EMPTY_SUBJECTS => "This work does not have any subjects. Click above to add associated subjects",
 }
