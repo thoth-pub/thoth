5aed3b9d7375062009848334e68be8c6bc420f6f
Javier Arias <javier@arias.re>
2020-11-09 10:45:28 +0000

    Implement pagination in publications component

--- thoth-app/src/component/publications.rs
+++ thoth-app/src/component/publications.rs
@@ -1,152 +1,21 @@
-use yew::html;
-use yew::prelude::*;
-use yew::ComponentLink;
-use yew_router::agent::RouteAgentDispatcher;
-use yew_router::agent::RouteRequest;
-use yew_router::route::Route;
-use yewtil::fetch::FetchAction;
-use yewtil::fetch::FetchState;
-use yewtil::future::LinkFuture;
-
-use crate::component::utils::Loader;
-use crate::component::utils::Reloader;
-use crate::models::publication::publications_query::DetailedPublication;
 use crate::models::publication::publications_query::FetchActionPublications;
 use crate::models::publication::publications_query::FetchPublications;
-use crate::route::AdminRoute;
-use crate::route::AppRoute;
+use crate::models::publication::publications_query::PublicationsRequest;
+use crate::models::publication::publications_query::PublicationsRequestBody;
+use crate::models::publication::publications_query::Variables;
+use crate::models::publication::publications_query::DetailedPublication;
 
-pub struct PublicationsComponent {
-    markdown: FetchPublications,
-    link: ComponentLink<Self>,
-    router: RouteAgentDispatcher<()>,
-}
-
-pub enum Msg {
-    SetMarkdownFetchState(FetchActionPublications),
-    GetMarkdown,
-    ChangeRoute(AppRoute),
-}
-
-impl Component for PublicationsComponent {
-    type Message = Msg;
-    type Properties = ();
-
-    fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
-        let router = RouteAgentDispatcher::new();
-
-        PublicationsComponent {
-            markdown: Default::default(),
-            link,
-            router,
-        }
-    }
-
-    fn rendered(&mut self, first_render: bool) {
-        if first_render {
-            self.link
-                .send_future(self.markdown.fetch(Msg::SetMarkdownFetchState));
-            self.link
-                .send_message(Msg::SetMarkdownFetchState(FetchAction::Fetching));
-        }
-    }
-
-    fn update(&mut self, msg: Self::Message) -> ShouldRender {
-        match msg {
-            Msg::SetMarkdownFetchState(fetch_state) => {
-                self.markdown.apply(fetch_state);
-                true
-            }
-            Msg::GetMarkdown => {
-                self.link
-                    .send_future(self.markdown.fetch(Msg::SetMarkdownFetchState));
-                self.link
-                    .send_message(Msg::SetMarkdownFetchState(FetchAction::Fetching));
-                false
-            }
-            Msg::ChangeRoute(r) => {
-                let route = Route::from(r);
-                self.router.send(RouteRequest::ChangeRoute(route));
-                false
-            }
-        }
-    }
-
-    fn change(&mut self, _props: Self::Properties) -> ShouldRender {
-        false
-    }
-
-    fn view(&self) -> Html {
-        match self.markdown.as_ref().state() {
-            FetchState::NotFetching(_) => {
-                html! {<Reloader onclick=self.link.callback(|_| Msg::GetMarkdown)/>}
-            }
-            FetchState::Fetching(_) => html! {<Loader/>},
-            FetchState::Fetched(body) => html! {
-                <>
-                    <nav class="level">
-                        <div class="level-left">
-                            <div class="level-item">
-                                <p class="subtitle is-5">
-                                    <strong>{ body.data.publications.iter().count() }</strong> { " publications" }
-                                </p>
-                            </div>
-                        </div>
-                        <div class="level-right">
-                            <p class="level-item">
-                                <article class="message is-info">
-                                    <div class="message-body">
-                                        { "New publications can be added directly to the work." }
-                                    </div>
-                                </article>
-                            </p>
-                        </div>
-                    </nav>
-                    <table class="table is-fullwidth is-hoverable">
-                        <thead>
-                            <tr>
-                                <th>{ "ID" }</th>
-                                <th>{ "Work Title" }</th>
-                                <th>{ "Work DOI" }</th>
-                                <th>{ "Publisher" }</th>
-                                <th>{ "Type" }</th>
-                                <th>{ "ISBN" }</th>
-                                <th>{ "URL" }</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            { for body.data.publications.iter().map(|p| self.render_publication(p)) }
-                        </tbody>
-                    </table>
-                </>
-            },
-            FetchState::Failed(_, err) => html! {&err},
-        }
-    }
-}
-
-impl PublicationsComponent {
-    fn change_route(&self, app_route: AppRoute) -> Callback<MouseEvent> {
-        self.link.callback(move |_| {
-            let route = app_route.clone();
-            Msg::ChangeRoute(route)
-        })
-    }
-
-    fn render_publication(&self, p: &DetailedPublication) -> Html {
-        html! {
-            <tr
-                class="row"
-                onclick=&self.change_route(AppRoute::Admin(AdminRoute::Publication(p.publication_id.clone())))
-            >
-                <td>{&p.publication_id}</td>
-                <td>{&p.work.title}</td>
-                <td>{&p.work.doi.clone().unwrap_or("".to_string())}</td>
-                <td>{&p.work.publisher()}</td>
-                <td>{&p.publication_type}</td>
-                <td>{&p.isbn.clone().unwrap_or("".to_string())}</td>
-                <td>{&p.publication_url.clone().unwrap_or("".to_string())}</td>
-            </tr>
-        }
-    }
+pagination_component! {
+    PublicationsComponent,
+    DetailedPublication,
+    publications,
+    publication_count,
+    PublicationsRequest,
+    FetchActionPublications,
+    FetchPublications,
+    PublicationsRequestBody,
+    Variables,
+    SEARCH_PUBLICATIONS,
+    PAGINATION_COUNT_PUBLICATIONS,
+    vec!["ID".to_string(), "Work Title".to_string(), "Work DOI".to_string(), "Publisher".to_string(), "Type".to_string(), "ISBN".to_string(), "URL".to_string()]
 }
--- thoth-app/src/string.rs
+++ thoth-app/src/string.rs
@@ -22,6 +22,7 @@ strings! {
     PAGINATION_COUNT_PUBLISHERS => "Displaying publishers",
     PAGINATION_COUNT_IMPRINTS => "Displaying imprints",
     PAGINATION_COUNT_CONTRIBUTORS => "Displaying contributors",
+    PAGINATION_COUNT_PUBLICATIONS => "Displaying publications",
     AUTHENTICATION_ERROR => "Authentication failed",
     RESPONSE_ERROR => "Failed to obtain a valid response from the server.",
     EMPTY_CONTRIBUTIONS => "This work does not have any contributions. Search contributors above to add its contributions.",
@@ -37,4 +38,5 @@ strings! {
     SEARCH_PUBLISHERS => "Search by publisher name or short name",
     SEARCH_IMPRINTS => "Search by imprint name or URL",
     SEARCH_CONTRIBUTORS => "Search by name or ORCID",
+    SEARCH_PUBLICATIONS => "Search by ISBN or URL",
 }
