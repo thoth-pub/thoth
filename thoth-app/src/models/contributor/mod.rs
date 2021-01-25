a43e300fbe9023eeca9914583668f31d07033afa
rhigman <73792779+rhigman@users.noreply.github.com>
2021-01-25 12:07:04 +0000

    Revert "[fail] Update app data structure to accept contributor's contribution data - loops infinitely"
    This reverts commit 44e07294aec2d0267a0066df35e3b338ebf8c968.

--- thoth-app/src/models/contributor/mod.rs
+++ thoth-app/src/models/contributor/mod.rs
@@ -5,7 +5,6 @@ use yew::prelude::Html;
 use yew::Callback;
 use yew::MouseEvent;
 
-use super::contribution::Contribution;
 use crate::route::AdminRoute;
 use crate::route::AppRoute;
 
@@ -18,7 +17,6 @@ pub struct Contributor {
     pub full_name: String,
     pub orcid: Option<String>,
     pub website: Option<String>,
-    pub contributions: Option<Vec<Contribution>>,
 }
 
 impl Contributor {
