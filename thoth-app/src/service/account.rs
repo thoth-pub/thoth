cb786d6b02b633abfa0f9b2865b43e889417eb8e
Javier Arias <javier@arias.re>
2021-02-16 09:58:36 +0000

    Remove redundant clone

--- thoth-app/src/service/account.rs
+++ thoth-app/src/service/account.rs
@@ -3,8 +3,8 @@ use yew_router::agent::RouteAgentDispatcher;
 use yew_router::agent::RouteRequest;
 use yew_router::route::Route;
 
-use crate::SESSION_KEY;
 use crate::route::AppRoute;
+use crate::SESSION_KEY;
 
 pub struct AccountService {
     login_route: Route,
@@ -15,9 +15,7 @@ const STORAGE_ERROR: &str = "local storage is disabled";
 impl AccountService {
     pub fn new() -> Self {
         let login_route = Route::from(AppRoute::Login);
-        Self {
-            login_route,
-        }
+        Self { login_route }
     }
 
     pub fn get_token(&self) -> Option<String> {
@@ -37,7 +35,7 @@ impl AccountService {
 
     fn update_storage(&self, token: Option<String>) {
         let mut storage_service = StorageService::new(Area::Local).expect(STORAGE_ERROR);
-        if let Some(t) = token.clone() {
+        if let Some(t) = token {
             storage_service.store(SESSION_KEY, Ok(t));
         } else {
             storage_service.remove(SESSION_KEY);
