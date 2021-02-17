7c6bf04885af92855e135c25618a51f6d72e9b53
rhigman <73792779+rhigman@users.noreply.github.com>
2021-02-17 11:57:11 +0000

    [POC] Retrieve permissions from token and check against user-supplied publisher ID before creating imprint

--- thoth-api/src/graphql/model.rs
+++ thoth-api/src/graphql/model.rs
@@ -7,7 +7,7 @@ use juniper::RootNode;
 use std::sync::Arc;
 use uuid::Uuid;
 
-use crate::account::model::DecodedToken;
+use crate::account::model::{AccountAccess, DecodedToken, LinkedPublisher};
 use crate::contribution::model::*;
 use crate::contributor::model::*;
 use crate::db::PgPool;
@@ -1920,6 +1920,29 @@ impl QueryRoot {
     }
 }
 
+fn get_user_permissions(token: &DecodedToken) -> AccountAccess {
+    let mut account_access = AccountAccess {
+        is_superuser: false,
+        is_bot: false,
+        linked_publishers: vec![],
+    };
+    if let Some(jwt) = &token.jwt {
+        account_access = jwt.namespace.clone();
+    }
+    account_access
+}
+
+fn id_in_linked_publishers(id: Uuid, linked_publishers: Vec<LinkedPublisher>) -> bool {
+    let mut id_found = false;
+    for publisher in linked_publishers {
+        if publisher.publisher_id == id {
+            id_found = true;
+            break;
+        }
+    }
+    id_found
+}
+
 pub struct MutationRoot;
 
 #[juniper::object(Context = Context)]
@@ -1952,6 +1975,13 @@ impl MutationRoot {
 
     fn create_imprint(context: &Context, data: NewImprint) -> FieldResult<Imprint> {
         context.token.jwt.as_ref().ok_or(ThothError::Unauthorised)?;
+
+        let account_access = get_user_permissions(&context.token);
+        if !account_access.is_superuser {
+            if !id_in_linked_publishers(data.publisher_id, account_access.linked_publishers) {
+                Err(ThothError::Unauthorised)?;
+            }
+        }
 
         let connection = context.db.get().unwrap();
         match diesel::insert_into(imprint::table)
