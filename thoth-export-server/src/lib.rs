d09cff760210a3de67a347e89380dcb112dfaa9c
Javier Arias <javier@jarias.org>
2021-05-21 12:46:24 +0100

    Add license information

--- thoth-export-server/src/lib.rs
+++ thoth-export-server/src/lib.rs
@@ -7,7 +7,7 @@ use paperclip::actix::{
     web::{self, HttpResponse, Json},
     Apiv2Schema, OpenApiExt,
 };
-use paperclip::v2::models::{DefaultApiRaw, Info, Tag, Contact};
+use paperclip::v2::models::{DefaultApiRaw, Info, Tag, Contact, License};
 use serde::{Deserialize, Serialize};
 use thoth_api::errors::ThothError;
 use thoth_client::work::get_work;
@@ -140,9 +140,13 @@ pub async fn start_server(host: String, port: String, gql_endpoint: String) -> i
                         .to_string(),
                 ),
                 contact: Some(Contact {
-                    name: Some("Thoth Team".to_string()),
+                    name: Some("Thoth Support".to_string()),
                     url: Some("https://thoth.pub".to_string()),
-                    email: Some("info@thoth.pub".to_string()),
+                    email: Some("support@thoth.pub".to_string()),
+                }),
+                license: Some(License {
+                    name: Some(env!("CARGO_PKG_LICENSE").parse().unwrap()),
+                    url: None
                 }),
                 ..Default::default()
             },
