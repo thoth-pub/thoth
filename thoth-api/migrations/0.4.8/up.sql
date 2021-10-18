f67c98cc208223aa8f6e7eff6aadcce7ab8ef537
rhigman <73792779+rhigman@users.noreply.github.com>
2021-10-18 10:41:17 +0100

    Enhance migrations to add further table constraints and convert location URLs back to publication_urls

--- thoth-api/migrations/0.4.8/up.sql
+++ thoth-api/migrations/0.4.8/up.sql
@@ -17,6 +17,14 @@ CREATE TABLE location (
 );
 SELECT diesel_manage_updated_at('location');
 
+-- Only allow one canonical location per publication
+CREATE UNIQUE INDEX location_uniq_canonical_true_idx ON location(publication_id)
+    WHERE canonical;
+
+-- Only allow one instance of each platform (except 'Other') per publication
+CREATE UNIQUE INDEX location_uniq_platform_idx ON location(publication_id,location_platform)
+    WHERE NOT location_platform = 'Other';
+
 CREATE TABLE location_history (
     location_history_id      UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     location_id              UUID NOT NULL REFERENCES location(location_id) ON DELETE CASCADE,
@@ -25,5 +33,12 @@ CREATE TABLE location_history (
     timestamp                TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
+-- Create location entries for every existing publication_url (assume all are landing pages)
 INSERT INTO location(publication_id, landing_page)
     SELECT publication_id, publication_url FROM publication WHERE publication_url IS NOT NULL;
+
+-- Only allow one publication of each type per work (existing data may breach this)
+-- To check for records which breach this constraint:
+-- `select * from publication a where (select count(*) from publication b where a.publication_type = b.publication_type and a.work_id = b.work_id) > 1 order by work_id, publication_type;`
+ALTER TABLE publication
+    ADD CONSTRAINT publication_publication_type_work_id_uniq UNIQUE (publication_type, work_id);
