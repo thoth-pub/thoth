6ea50d29302d3959c335654d45d822d5c8f9890f
Javier Arias <javier@jarias.org>
2022-02-04 12:57:17 +0000

    Add series new fields to series component

--- thoth-app/src/component/series.rs
+++ thoth-app/src/component/series.rs
@@ -24,6 +24,7 @@ use crate::component::delete_dialogue::ConfirmDeleteComponent;
 use crate::component::utils::FormImprintSelect;
 use crate::component::utils::FormSeriesTypeSelect;
 use crate::component::utils::FormTextInput;
+use crate::component::utils::FormTextarea;
 use crate::component::utils::FormUrlInput;
 use crate::component::utils::Loader;
 use crate::models::imprint::imprints_query::FetchActionImprints;
@@ -264,6 +265,8 @@ impl Component for SeriesComponent {
                         issn_print: self.series.issn_print.clone(),
                         issn_digital: self.series.issn_digital.clone(),
                         series_url: self.series.series_url.clone(),
+                        series_description: self.series.series_description.clone(),
+                        series_cfp_url: self.series.series_cfp_url.clone(),
                         imprint_id: self.series.imprint.imprint_id,
                     },
                     ..Default::default()
@@ -339,6 +342,8 @@ impl Component for SeriesComponent {
                 .issn_digital
                 .neq_assign(issn_digital.trim().to_owned()),
             Msg::ChangeSeriesUrl(value) => self.series.series_url.neq_assign(value.to_opt_string()),
+            Msg::ChangeSeriesDescription(value) => self.series.series_description.neq_assign(value.to_opt_string()),
+            Msg::ChangeSeriesCfpUrl(value) => self.series.series_cfp_url.neq_assign(value.to_opt_string()),
             Msg::ChangeRoute(r) => {
                 let route = Route::from(r);
                 self.router.send(RouteRequest::ChangeRoute(route));
@@ -432,6 +437,16 @@ impl Component for SeriesComponent {
                                 label = "Series URL"
                                 value=self.series.series_url.clone()
                                 oninput=self.link.callback(|e: InputData| Msg::ChangeSeriesUrl(e.value))
+                            />
+                            <FormUrlInput
+                                label = "Series Call for Proposals URL"
+                                value=self.series.series_cfp_url.clone()
+                                oninput=self.link.callback(|e: InputData| Msg::ChangeSeriesCfpUrl(e.value))
+                            />
+                            <FormTextarea
+                                label = "Series Description"
+                                value=self.series.series_description.clone()
+                                oninput=self.link.callback(|e: InputData| Msg::ChangeSeriesDescription(e.value))
                             />
 
                             <div class="field">
