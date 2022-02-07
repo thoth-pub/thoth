3603c29abc394633a0a9eff6bac8cbe9f13a0db0
Javier Arias <javier@jarias.org>
2022-02-07 15:16:51 +0000

    Drop unsupported values

--- thoth-api/migrations/0.7.2/down.sql
+++ thoth-api/migrations/0.7.2/down.sql
@@ -2,6 +2,10 @@ ALTER TABLE series
     DROP COLUMN series_description,
     DROP COLUMN series_cfp_url;
 
+-- We cannot drop individual enum values - we must drop the type and recreate it
+--
+-- Delete publications with about-to-be-dropped types
+DELETE FROM publication WHERE publication_type IN ('AZW3', 'DOCX', 'FictionBook');
 ALTER TABLE publication ALTER publication_type TYPE text;
 DROP TYPE publication_type;
 CREATE TYPE publication_type AS ENUM (
@@ -13,4 +17,4 @@ CREATE TYPE publication_type AS ENUM (
     'Epub',
     'Mobi'
 );
-ALTER TABLE publication ALTER publication_type TYPE publication_type USING publication::publication_type;
+ALTER TABLE publication ALTER publication_type TYPE publication_type USING publication_type::publication_type;
