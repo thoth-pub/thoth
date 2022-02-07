8b5ec49994b17ba13e13facecbf74e649dfeafde
Javier Arias <javier@jarias.org>
2022-02-07 10:34:14 +0000

    Fix typo in migration comment

--- thoth-api/migrations/0.7.2/up.sql
+++ thoth-api/migrations/0.7.2/up.sql
@@ -1,5 +1,5 @@
 ALTER TABLE series
-    -- Descrition of the series
+    -- Description of the series
     ADD COLUMN series_description TEXT CHECK (octet_length(series_description) >= 1),
     -- Call for proposals URL
     ADD COLUMN series_cfp_url TEXT CHECK (series_cfp_url ~* '^[^:]*:\/\/(?:[^\/:]*:[^\/@]*@)?(?:[^\/:.]*\.)+([^:\/]+)');
