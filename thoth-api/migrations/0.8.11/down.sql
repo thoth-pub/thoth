f12677931cda15058f421d8ba7ccf961fbe32317
Javier Arias <javier@jarias.org>
2022-09-30 14:50:29 +0100

    Rename funder constraints

--- thoth-api/migrations/0.8.11/down.sql
+++ thoth-api/migrations/0.8.11/down.sql
@@ -1,2 +1,7 @@
 ALTER TABLE work DROP CONSTRAINT work_place_check;
 ALTER TABLE work ADD CONSTRAINT work_reference_check1 CHECK (octet_length(reference) >= 1);
+
+ALTER TABLE institution RENAME CONSTRAINT institution_pkey TO funder_pkey;
+ALTER TABLE institution RENAME CONSTRAINT institution_doi_uniq_idx TO funder_doi_uniq_idx;
+ALTER TABLE institution RENAME CONSTRAINT institution_funder_doi_check TO funder_funder_doi_check;
+ALTER TABLE institution RENAME CONSTRAINT institution_institution_name_check TO funder_funder_name_check;
--- thoth-api/migrations/0.8.11/up.sql
+++ thoth-api/migrations/0.8.11/up.sql
@@ -1,2 +1,7 @@
 ALTER TABLE work DROP CONSTRAINT work_reference_check1;
 ALTER TABLE work ADD CONSTRAINT work_place_check CHECK (octet_length(place) >= 1);
+
+ALTER TABLE institution RENAME CONSTRAINT funder_pkey TO institution_pkey;
+ALTER TABLE institution RENAME CONSTRAINT funder_doi_uniq_idx TO institution_doi_uniq_idx;
+ALTER TABLE institution RENAME CONSTRAINT funder_funder_doi_check TO institution_institution_doi_check;
+ALTER TABLE institution RENAME CONSTRAINT funder_funder_name_check TO institution_institution_name_check;
