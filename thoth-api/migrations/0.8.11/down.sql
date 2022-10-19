46b1efbe4d73c3bac92c7d817f3e5df4d54c5c56
Javier Arias <javier@jarias.org>
2022-10-19 10:21:04 +0100

    Fix constraint name

--- thoth-api/migrations/0.8.11/down.sql
+++ thoth-api/migrations/0.8.11/down.sql
@@ -3,5 +3,5 @@ ALTER TABLE work ADD CONSTRAINT work_reference_check1 CHECK (octet_length(refere
 
 ALTER TABLE institution RENAME CONSTRAINT institution_pkey TO funder_pkey;
 ALTER INDEX institution_doi_uniq_idx RENAME TO funder_doi_uniq_idx;
-ALTER TABLE institution RENAME CONSTRAINT institution_funder_doi_check TO funder_funder_doi_check;
+ALTER TABLE institution RENAME CONSTRAINT institution_institution_doi_check TO funder_funder_doi_check;
 ALTER TABLE institution RENAME CONSTRAINT institution_institution_name_check TO funder_funder_name_check;
