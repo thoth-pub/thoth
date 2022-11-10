9123077d57e762fd7799987682d18b6dbf6ec36b
rhigman <73792779+rhigman@users.noreply.github.com>
2022-11-10 11:14:58 +0000

    Simplify JSON output specification to avoid unnecessary bytes/string reconversions

--- thoth-export-server/src/json/json_thoth.rs
+++ thoth-export-server/src/json/json_thoth.rs
@@ -1,34 +1,23 @@
-use std::io::Write;
 use thoth_client::Work;
 use thoth_errors::{ThothError, ThothResult};
 
-use super::{JsonEntry, JsonSpecification};
+use super::JsonSpecification;
 
 #[derive(Copy, Clone)]
 pub(crate) struct JsonThoth;
 
 impl JsonSpecification for JsonThoth {
-    fn handle_event(w: &mut Vec<u8>, works: &[Work]) -> ThothResult<()> {
+    fn handle_event(works: &[Work]) -> ThothResult<String> {
         match works.len() {
             0 => Err(ThothError::IncompleteMetadataRecord(
                 "json::thoth".to_string(),
                 "Not enough data".to_string(),
             )),
-            1 => JsonEntry::<JsonThoth>::json_entry(works.first().unwrap(), w),
+            1 => serde_json::to_string_pretty(works.first().unwrap())
+                .map_err(|e| ThothError::InternalError(e.to_string())),
             // handler::by_publisher() prevents generation of output for multiple records
             _ => unreachable!(),
         }
-    }
-}
-
-impl JsonEntry<JsonThoth> for Work {
-    fn json_entry(&self, w: &mut Vec<u8>) -> ThothResult<()> {
-        w.write_all(
-            serde_json::to_string_pretty(self)
-                .expect("Could not create JSON")
-                .as_bytes(),
-        )?;
-        Ok(())
     }
 }
 
--- thoth-export-server/src/json/mod.rs
+++ thoth-export-server/src/json/mod.rs
@@ -1,22 +1,12 @@
 use thoth_client::Work;
-use thoth_errors::{ThothError, ThothResult};
+use thoth_errors::ThothResult;
 
 pub(crate) trait JsonSpecification {
     fn generate(&self, works: &[Work]) -> ThothResult<String> {
-        let mut buffer: Vec<u8> = Vec::new();
-        Self::handle_event(&mut buffer, works)
-            .map(|_| buffer)
-            .and_then(|json| {
-                String::from_utf8(json)
-                    .map_err(|_| ThothError::InternalError("Could not parse JSON".to_string()))
-            })
+        Self::handle_event(works)
     }
 
-    fn handle_event(w: &mut Vec<u8>, works: &[Work]) -> ThothResult<()>;
-}
-
-pub(crate) trait JsonEntry<T: JsonSpecification> {
-    fn json_entry(&self, w: &mut Vec<u8>) -> ThothResult<()>;
+    fn handle_event(works: &[Work]) -> ThothResult<String>;
 }
 
 mod json_thoth;
