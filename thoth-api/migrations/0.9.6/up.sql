9045219b5e085d8141159563353c2c2b38a043fa
Javier Arias <javier@jarias.org>
2023-01-31 12:01:04 +0000

    Update DOI checks in reference and institution tables

--- thoth-api/migrations/0.9.6/up.sql
+++ thoth-api/migrations/0.9.6/up.sql
@@ -1,3 +1,11 @@
 ALTER TABLE work DROP CONSTRAINT work_doi_check;
 ALTER TABLE work ADD CONSTRAINT work_doi_check
   CHECK (doi ~ '^https:\/\/doi\.org\/10\.\d{4,9}\/[-._\;\(\)\[\]\/:a-zA-Z0-9]+$');
+
+ALTER TABLE reference DROP CONSTRAINT reference_doi_check;
+ALTER TABLE reference ADD CONSTRAINT reference_doi_check
+  CHECK (doi ~ '^https:\/\/doi\.org\/10\.\d{4,9}\/[-._\;\(\)\[\]\/:a-zA-Z0-9]+$');
+
+ALTER TABLE institution DROP CONSTRAINT institution_institution_doi_check;
+ALTER TABLE institution ADD CONSTRAINT institution_institution_doi_check
+  CHECK (doi ~ '^https:\/\/doi\.org\/10\.\d{4,9}\/[-._\;\(\)\[\]\/:a-zA-Z0-9]+$');
