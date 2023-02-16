ddf265434844f1f12c9eda51482cd318b60e8acc
Javier Arias <javier@jarias.org>
2023-02-16 15:33:58 +0000

    Use concurrent requests

--- thoth-export-server/src/specification_query.rs
+++ thoth-export-server/src/specification_query.rs
@@ -1,3 +1,4 @@
+use futures::{stream, StreamExt, TryStreamExt};
 use std::convert::{TryFrom, TryInto};
 use std::sync::Arc;
 use thoth_client::{QueryParameters, ThothClient, Work};
@@ -5,6 +6,9 @@ use thoth_errors::{ThothError, ThothResult};
 use uuid::Uuid;
 
 use crate::record::MetadataSpecification;
+
+const CONCURRENT_REQUESTS: usize = 4;
+const PAGINATION_LIMIT: i64 = 100;
 
 enum SpecificationRequest {
     ByWork,
@@ -42,23 +46,38 @@ impl SpecificationQuery {
         let parameters: QueryParameters =
             QueryConfiguration::by_publisher(self.specification).try_into()?;
 
+        // get the total work count to figure out how to paginate the results
         let work_count = self
             .thoth_client
             .get_work_count(Some(vec![publisher_id]))
             .await?;
-        let mut works: Vec<Work> = vec![];
-        let mut current_iteration: i64 = 1;
-        let limit: i64 = 100;
-        let total_iterations = (work_count / limit) + 1;
+        let total_pages = (work_count / PAGINATION_LIMIT) + 1;
+        // get a vector of all page offsets we will need
+        let offsets = (1..total_pages)
+            .map(|current_page| (current_page - 1) * PAGINATION_LIMIT)
+            .collect::<Vec<i64>>();
 
-        while current_iteration <= total_iterations {
-            let offset = (current_iteration - 1) * limit;
-            let works_page = self
-                .thoth_client
-                .get_works(Some(vec![publisher_id]), limit, offset, parameters)
-                .await?;
-            works.extend(works_page);
-            current_iteration += 1;
+        // make concurrent requests iterating the list of offsets to asynchronously obtain all pages
+        let mut works_pages = stream::iter(offsets)
+            .map(|offset| {
+                let client = &self.thoth_client;
+                async move {
+                    client
+                        .get_works(
+                            Some(vec![publisher_id]),
+                            PAGINATION_LIMIT,
+                            offset,
+                            parameters,
+                        )
+                        .await
+                }
+            })
+            .buffer_unordered(CONCURRENT_REQUESTS);
+
+        // merge all pages
+        let mut works: Vec<Work> = vec![];
+        while let Some(page) = works_pages.try_next().await? {
+            works.extend(page);
         }
         Ok(works)
     }
