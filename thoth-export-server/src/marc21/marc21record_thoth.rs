84a2a338100ed296f185c58da99e59511903e2d6
Javier Arias <javier@jarias.org>
2023-03-27 15:44:39 +0100

    Add title fields and cover

--- thoth-export-server/src/marc21/marc21record_thoth.rs
+++ thoth-export-server/src/marc21/marc21record_thoth.rs
@@ -1,6 +1,7 @@
 use crate::marc21::Marc21Field;
-use marc::{Record, RecordBuilder};
-use thoth_client::{Work, WorkPublications, WorkType};
+use marc::{FieldRepr, Record, RecordBuilder};
+use thoth_api::model::contribution::ContributionType;
+use thoth_client::{Work, WorkContributions, WorkPublications, WorkType};
 use thoth_errors::{ThothError, ThothResult};
 
 use super::{Marc21Entry, Marc21Specification};
@@ -35,32 +36,35 @@ impl Marc21Entry<Marc21RecordThoth> for Work {
         let mut builder = RecordBuilder::new();
 
         // 245 – title
-        let mut title_subfield_data: Vec<u8> = Vec::new();
-        title_subfield_data.extend(b"00"); // no title added entry
-        title_subfield_data.extend(b"a"); // main title
-        title_subfield_data.extend(self.title.clone().into_bytes());
-        title_subfield_data.extend(b"h"); // general material designation (GMD)
-        title_subfield_data.extend(b"[electronic resource] :");
+        let mut title_field: FieldRepr = FieldRepr::from((b"245", "00")); // no title added entry
+        title_field = title_field.add_subfield(b'a', self.title.clone().into_bytes())?; // main title
+        title_field = title_field.add_subfield(b'h', b"[electronic resource] :")?; // general material designation (GMD)
         if let Some(subtitle) = self.subtitle.clone() {
-            title_subfield_data.extend(b"b"); // main title
-            title_subfield_data.extend(subtitle.into_bytes());
+            title_field = title_field.add_subfield(b'b', subtitle.into_bytes())?;
+            // subtitle
         }
-        builder.add_field((b"245", title_subfield_data))?;
+        title_field =
+            title_field.add_subfield(b'c', contributors_string(&self.contributions).as_bytes())?; // statement of responsibility
+        builder.add_field(title_field)?;
 
-        let publications: Vec<WorkPublications> = self
-            .publications
-            .clone()
-            .into_iter()
-            .filter(|p| p.isbn.is_some())
-            .collect();
-        if !publications.is_empty() {
-            for publication in &publications {
-                Marc21Field::<Marc21RecordThoth>::to_field(publication, &mut builder)?;
-            }
+        // 020 - ISBN
+        for publication in &self.publications {
+            Marc21Field::<Marc21RecordThoth>::to_field(publication, &mut builder)?;
         }
 
-        if let Some(doi) = self.doi.clone() {
-            builder.add_field((b"024", doi.to_lowercase_string().into_bytes()))?;
+        // 856 - location
+        if let Some(doi) = &self.doi {
+            let mut cover_field: FieldRepr = FieldRepr::from((b"856", "40")); // version of resource
+            cover_field = cover_field.add_subfield(b"u", doi.to_lowercase_string().into_bytes())?;
+            cover_field = cover_field.add_subfield(b"z", b"Connect to e-book")?;
+            builder.add_field(cover_field)?;
+        }
+        // 856 - cover
+        if let Some(cover_url) = self.cover_url.clone() {
+            let mut cover_field: FieldRepr = FieldRepr::from((b"856", "42")); // related resource
+            cover_field = cover_field.add_subfield(b"u", cover_url.into_bytes())?;
+            cover_field = cover_field.add_subfield(b"z", b"Connect to cover image")?;
+            builder.add_field(cover_field)?;
         }
 
         Ok(builder.get_record()?)
@@ -69,15 +73,51 @@ impl Marc21Entry<Marc21RecordThoth> for Work {
 
 impl Marc21Field<Marc21RecordThoth> for WorkPublications {
     fn to_field(&self, builder: &mut RecordBuilder) -> ThothResult<()> {
-        if let Some(isbn) = self.isbn.clone() {
-            let mut subfield_data: Vec<u8> = Vec::new();
-            subfield_data.extend(b"a");
-            subfield_data.extend(isbn.to_hyphenless_string().as_bytes());
-            subfield_data.extend(b"q");
-            subfield_data.extend(format!("({})", self.publication_type).into_bytes());
+        if let Some(isbn) = &self.isbn {
+            let mut isbn_field: FieldRepr = FieldRepr::from((b"020", ""));
+            isbn_field = isbn_field.add_subfield(b'a', isbn.to_hyphenless_string().as_bytes())?;
+            isbn_field = isbn_field
+                .add_subfield(b'q', format!("({})", self.publication_type).into_bytes())?;
 
-            builder.add_field((b"020", subfield_data))?;
+            builder.add_field(isbn_field)?;
         }
         Ok(())
     }
+}
+
+fn contributors_string(contributions: &[WorkContributions]) -> String {
+    // group main contributions by contribution type
+    let mut contributions_by_type: std::collections::HashMap<
+        ContributionType,
+        Vec<&WorkContributions>,
+    > = std::collections::HashMap::new();
+    for c in contributions.iter().filter(|c| c.main_contribution) {
+        let entry = contributions_by_type
+            .entry(ContributionType::from(c.contribution_type.clone()))
+            .or_insert(vec![]);
+        entry.push(c);
+    }
+
+    // build string for each contribution type
+    let mut type_strings = vec![];
+    for (contribution_type, contributions) in contributions_by_type.iter() {
+        let names = contributions
+            .iter()
+            .map(|c| c.full_name.clone())
+            .collect::<Vec<_>>()
+            .join(", ");
+
+        let type_string = match contribution_type {
+            ContributionType::Author => names,
+            ContributionType::Editor => format!("edited by {}", names),
+            _ => format!("{} ({})", contribution_type, names),
+        };
+        type_strings.push(type_string);
+    }
+
+    // join type strings with appropriate separators
+    let mut result = type_strings.join("; ");
+    result.push('.');
+
+    result
 }
