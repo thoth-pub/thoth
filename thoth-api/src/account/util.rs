93906aae79a7c4e92834bab653cdf46771825573
Javier Arias <javier@jarias.org>
2023-09-04 08:17:07 +0100

    Upgrade rand

--- thoth-api/src/account/util.rs
+++ thoth-api/src/account/util.rs
@@ -12,7 +12,7 @@ pub fn make_salt() -> String {
 
     let password: String = (0..PASSWORD_LEN)
         .map(|_| {
-            let idx = rng.gen_range(0, CHARSET.len());
+            let idx = rng.gen_range(0..CHARSET.len());
             CHARSET[idx] as char
         })
         .collect();
