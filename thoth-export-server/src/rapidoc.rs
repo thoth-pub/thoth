9deb9c6b5210cfe47827a7f9b9b0bf2012600779
Javier Arias <javier@jarias.org>
2024-04-08 19:06:40 +0100

    Update rapidoc style

--- thoth-export-server/src/rapidoc.rs
+++ thoth-export-server/src/rapidoc.rs
@@ -28,6 +28,13 @@ pub fn rapidoc_source(openapi_spec: &str) -> String {
         <meta name="msapplication-TileImage" content="https://cdn.thoth.pub/ms-icon-144x144.png">
         <meta name="theme-color" content="#FFDD57">
         <script type="module" src="https://unpkg.com/rapidoc/dist/rapidoc-min.js"></script>
+        <style>
+            rapi-doc::part(anchor){{ color: #7b377b; }}
+            rapi-doc::part(section-overview-title){{ color: #007944; }}
+            rapi-doc::part(section-tag-title){{ color: #007944; }}
+            rapi-doc::part(label-operation-method){{ color: #007944; }}
+            rapi-doc::part(section-navbar-tag){{ color: #007944; }}
+        </style>
     </head>
     <body>
       <rapi-doc
@@ -37,6 +44,11 @@ pub fn rapidoc_source(openapi_spec: &str) -> String {
         show-header = "false"
         allow-authentication = "false"
         allow-server-selection = "false"
+        primary-color = "#8c3f8d"
+        nav-bg-color = "#ffdd57"
+        bg-color = "#fffcf2"
+        text-color = "#111827"
+        font-size = "large"
       >
         <img
             slot="nav-logo"
