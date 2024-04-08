d4fe39bc107faaa51641dfdce0f772d38f840d0a
Javier Arias <javier@jarias.org>
2024-04-08 19:12:12 +0100

    Add icons to graphiql

--- thoth-api-server/src/graphiql.rs
+++ thoth-api-server/src/graphiql.rs
@@ -53,14 +53,32 @@ pub fn graphiql_source(graphql_endpoint_url: &str) -> String {
 "#;
 
     format!(
-        r#"
+        r##"
 <!DOCTYPE html>
 <html>
     <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>Thoth GraphQL API Documentation</title>
-        <link rel="shortcut icon" href="https://thoth.pub/favicon.ico" />
+        <link rel="shortcut icon" href="https://cdn.thoth.pub/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="57x57" href="https://cdn.thoth.pub/apple-icon-57x57.png">
+        <link rel="apple-touch-icon" sizes="60x60" href="https://cdn.thoth.pub/apple-icon-60x60.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="https://cdn.thoth.pub/apple-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="76x76" href="https://cdn.thoth.pub/apple-icon-76x76.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="https://cdn.thoth.pub/apple-icon-114x114.png">
+        <link rel="apple-touch-icon" sizes="120x120" href="https://cdn.thoth.pub/apple-icon-120x120.png">
+        <link rel="apple-touch-icon" sizes="144x144" href="https://cdn.thoth.pub/apple-icon-144x144.png">
+        <link rel="apple-touch-icon" sizes="152x152" href="https://cdn.thoth.pub/apple-icon-152x152.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="https://cdn.thoth.pub/apple-icon-180x180.png">
+        <link rel="icon" type="image/png" sizes="192x192"  href="https://cdn.thoth.pub/android-icon-192x192.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="https://cdn.thoth.pub/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="96x96" href="https://cdn.thoth.pub/favicon-96x96.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="https://cdn.thoth.pub/favicon-16x16.png">
+        <link rel="manifest" href="https://cdn.thoth.pub/manifest.json">
+        <link rel="manifest" href="https://cdn.thoth.pub/manifest.json">
+        <meta name="msapplication-TileColor" content="#FFDD57">
+        <meta name="msapplication-TileImage" content="https://cdn.thoth.pub/ms-icon-144x144.png">
+        <meta name="theme-color" content="#FFDD57">
         <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/graphiql@0.17.5/graphiql.min.css">
           <style>
             html, body, #app {{
@@ -106,6 +124,6 @@ pub fn graphiql_source(graphql_endpoint_url: &str) -> String {
         </script>
     </body>
 </html>
-"#,
+"##,
     )
 }
