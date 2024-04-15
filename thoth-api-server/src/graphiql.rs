301a8a65c295295be25ebbcdfc0d5bb4f14c1a56
Javier Arias <javier@jarias.org>
2024-04-15 11:56:45 +0100

    Update graphiql

--- thoth-api-server/src/graphiql.rs
+++ thoth-api-server/src/graphiql.rs
@@ -2,7 +2,7 @@
 
 /// Generate the HTML source to show a GraphiQL interface
 pub fn graphiql_source(graphql_endpoint_url: &str) -> String {
-    let default_query = r#"# Welcome to Thoth's GraphQL API explorer (GraphiQL).
+    let default_query = r##"# Welcome to Thoth's GraphQL API explorer (GraphiQL).
 #
 # GraphiQL is an in-browser tool for writing, validating, and
 # testing GraphQL queries.
@@ -11,31 +11,16 @@ pub fn graphiql_source(graphql_endpoint_url: &str) -> String {
 # typeaheads aware of the current GraphQL type schema and live syntax and
 # validation errors highlighted within the text.
 #
-# Click on the QueryRoot in the Documentation Explorer ( < Docs ) on the
-# right hand side of the screen to navigate the API schema.
+# Click on the QueryRoot in the Documentation Explorer (first icon) on
+# the top left of the screen to navigate the API schema.
 #
 # GraphQL queries typically start with a "{" character. Lines that starts
-# with a # are ignored.
+# with a "#" are ignored.
 #
-# An example Thoth GraphQL query might look like:
-#
-#     {
-#       books(workStatuses: [ACTIVE]) {
-#           fullTitle
-#           doi
-#       }
-#     }
-#
-# Keyboard shortcuts:
+# Run the following example Thoth GraphQL query:
 #
 #       Run Query:  Ctrl-Enter (or press the play button above)
 #
-#   Auto Complete:  Ctrl-Space (or just start typing)
-#
-#
-# Run the following query (Ctrl-Enter):
-#
-
 {
     books(order: {field: PUBLICATION_DATE, direction: ASC}) {
        fullTitle
@@ -50,80 +35,93 @@ pub fn graphiql_source(graphql_endpoint_url: &str) -> String {
        }
     }
 }
-"#;
+"##;
 
     format!(
         r##"
 <!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>Thoth GraphQL API Documentation</title>
-        <link rel="shortcut icon" href="https://cdn.thoth.pub/favicon.ico" />
-        <link rel="apple-touch-icon" sizes="57x57" href="https://cdn.thoth.pub/apple-icon-57x57.png">
-        <link rel="apple-touch-icon" sizes="60x60" href="https://cdn.thoth.pub/apple-icon-60x60.png">
-        <link rel="apple-touch-icon" sizes="72x72" href="https://cdn.thoth.pub/apple-icon-72x72.png">
-        <link rel="apple-touch-icon" sizes="76x76" href="https://cdn.thoth.pub/apple-icon-76x76.png">
-        <link rel="apple-touch-icon" sizes="114x114" href="https://cdn.thoth.pub/apple-icon-114x114.png">
-        <link rel="apple-touch-icon" sizes="120x120" href="https://cdn.thoth.pub/apple-icon-120x120.png">
-        <link rel="apple-touch-icon" sizes="144x144" href="https://cdn.thoth.pub/apple-icon-144x144.png">
-        <link rel="apple-touch-icon" sizes="152x152" href="https://cdn.thoth.pub/apple-icon-152x152.png">
-        <link rel="apple-touch-icon" sizes="180x180" href="https://cdn.thoth.pub/apple-icon-180x180.png">
-        <link rel="icon" type="image/png" sizes="192x192"  href="https://cdn.thoth.pub/android-icon-192x192.png">
-        <link rel="icon" type="image/png" sizes="32x32" href="https://cdn.thoth.pub/favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="96x96" href="https://cdn.thoth.pub/favicon-96x96.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="https://cdn.thoth.pub/favicon-16x16.png">
-        <link rel="manifest" href="https://cdn.thoth.pub/manifest.json">
-        <link rel="manifest" href="https://cdn.thoth.pub/manifest.json">
-        <meta name="msapplication-TileColor" content="#FFDD57">
-        <meta name="msapplication-TileImage" content="https://cdn.thoth.pub/ms-icon-144x144.png">
-        <meta name="theme-color" content="#FFDD57">
-        <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/graphiql@0.17.5/graphiql.min.css">
-          <style>
-            html, body, #app {{
-                height: 100vh;
-                margin: 0;
-                overflow: hidden;
-            }}
-        </style>
-    </head>
-    <body>
-        <div id="app"></div>
-        <script src="//cdnjs.cloudflare.com/ajax/libs/fetch/2.0.3/fetch.js"></script>
-        <script src="//cdnjs.cloudflare.com/ajax/libs/react/16.10.2/umd/react.production.min.js"></script>
-        <script src="//cdnjs.cloudflare.com/ajax/libs/react-dom/16.10.2/umd/react-dom.production.min.js"></script>
-        <script src="//cdn.jsdelivr.net/npm/graphiql@0.17.5/graphiql.min.js"></script>
-        <script>var GRAPHQL_URL = '{graphql_endpoint_url}';</script>
-        <script>
-            function graphQLFetcher(params) {{
-                return fetch(GRAPHQL_URL, {{
-                    method: 'post',
-                    headers: {{
-                        'Accept': 'application/json',
-                        'Content-Type': 'application/json',
-                    }},
-                    credentials: 'include',
-                    body: JSON.stringify(params)
-                }}).then(function (response) {{
-                    return response.text();
-                }}).then(function (body) {{
-                    try {{
-                        return JSON.parse(body);
-                    }} catch (error) {{
-                        return body;
-                    }}
-                }});
-            }}
-            ReactDOM.render(
-                React.createElement(GraphiQL, {{
-                    fetcher: graphQLFetcher,
-                    query: String.raw`{default_query}`,
-                }}),
-                document.querySelector('#app'));
-        </script>
-    </body>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Thoth GraphQL API Documentation</title>
+    <link rel="shortcut icon" href="https://cdn.thoth.pub/favicon.ico" />
+    <link rel="apple-touch-icon" sizes="57x57" href="https://cdn.thoth.pub/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="https://cdn.thoth.pub/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="https://cdn.thoth.pub/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="https://cdn.thoth.pub/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="https://cdn.thoth.pub/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="https://cdn.thoth.pub/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="https://cdn.thoth.pub/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="https://cdn.thoth.pub/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="https://cdn.thoth.pub/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="https://cdn.thoth.pub/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="https://cdn.thoth.pub/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="https://cdn.thoth.pub/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="https://cdn.thoth.pub/favicon-16x16.png">
+    <link rel="manifest" href="https://cdn.thoth.pub/manifest.json">
+    <meta name="msapplication-TileColor" content="#FFDD57">
+    <meta name="msapplication-TileImage" content="https://cdn.thoth.pub/ms-icon-144x144.png">
+    <meta name="theme-color" content="#FFDD57">
+    <script crossorigin src="https://unpkg.com/react@18/umd/react.development.js"></script>
+    <script crossorigin src="https://unpkg.com/react-dom@18/umd/react-dom.development.js"></script>
+    <script src="https://unpkg.com/graphiql@3.2/graphiql.min.js" type="application/javascript"></script>
+    <link rel="stylesheet" href="https://unpkg.com/graphiql@3.2/graphiql.min.css" />
+    <script crossorigin src="https://unpkg.com/@graphiql/plugin-explorer@2/dist/index.umd.js"></script>
+    <link rel="stylesheet" href="https://unpkg.com/@graphiql/plugin-explorer@2/dist/style.css"/>
+    <style>
+     .graphiql-container,
+      .CodeMirror-info,
+      .CodeMirror-lint-tooltip,
+      .graphiql-dialog,
+      .graphiql-dialog-overlay,
+      .graphiql-tooltip,
+      [data-radix-popper-content-wrapper] {{
+        --color-primary: 298, 38%, 40%;
+        --color-secondary: 154, 100%, 24%;
+        --color-tertiary: 320, 95%, 43%;
+        --color-info: 188, 100%, 36%;
+        --color-warning: 24, 100%, 63%;
+        --color-base: 46, 100%, 97%;
+      }}
+      body {{
+        height: 100%;
+        margin: 0;
+        width: 100%;
+        overflow: hidden;
+      }}
+
+      #graphiql {{
+        height: 100vh;
+      }}
+    </style>
+  </head>
+
+  <body>
+    <div id="graphiql">Loading...</div>
+    <script>
+      const root = ReactDOM.createRoot(document.getElementById('graphiql'));
+      const fetcher = GraphiQL.createFetcher({{
+        url: '{graphql_endpoint_url}',
+        method: 'post',
+        headers: {{
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        }},
+      }});
+      const explorerPlugin = GraphiQLPluginExplorer.explorerPlugin();
+      root.render(
+        React.createElement(GraphiQL, {{
+          fetcher,
+          query: String.raw`{default_query}`,
+          defaultEditorToolsVisibility: false,
+          plugins: [explorerPlugin],
+        }}),
+      );
+    </script>
+  </body>
 </html>
+
 "##,
     )
 }
