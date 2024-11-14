5c865ac9761b980082a370c33e923680b3dea130
Javier Arias <javier@jarias.org>
2024-11-14 10:47:29 +0000

    Implement deadpool redis

--- thoth-api/src/redis.rs
+++ thoth-api/src/redis.rs
@@ -1,13 +1,33 @@
+use deadpool_redis::{redis::AsyncCommands, Config, Connection, Pool};
 use dotenv::dotenv;
-pub use redis::{AsyncCommands, Client as RedisClient};
 use std::env;
+use thoth_errors::{ThothError, ThothResult};
 
+pub type RedisPool = Pool;
+type RedisConnection = Connection;
+
+pub fn init_pool() -> RedisPool {
+    Config::from_url(get_redis_url())
+        .builder()
+        .expect("Failed to create redis pool.")
+        .build()
+        .expect("Failed to build redis pool.")
+}
 fn get_redis_url() -> String {
     dotenv().ok();
     env::var("REDIS_URL").expect("REDIS_URL must be set")
 }
 
-pub fn redis_client() -> RedisClient {
-    let redis_url = get_redis_url();
-    RedisClient::open(redis_url).expect("Failed to open redis client")
+async fn create_connection(pool: &RedisPool) -> ThothResult<RedisConnection> {
+    pool.get().await.map_err(ThothError::from)
+}
+
+pub async fn set(pool: &RedisPool, key: &str, value: &str) -> ThothResult<()> {
+    let mut con = create_connection(pool).await?;
+    con.set(key, value).await.map_err(ThothError::from)
+}
+
+pub async fn get(pool: &RedisPool, key: &str) -> ThothResult<String> {
+    let mut con = create_connection(pool).await?;
+    con.get(key).await.map_err(ThothError::from)
 }
