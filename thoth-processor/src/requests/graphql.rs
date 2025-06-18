c3829103800ee13ed20e6079053c576535b272c4
rhigman <73792779+rhigman@users.noreply.github.com>
2025-06-18 14:29:02 +0100

    Use appropriate structures for deserialising response

--- thoth-processor/src/requests/graphql.rs
+++ thoth-processor/src/requests/graphql.rs
@@ -18,7 +18,46 @@ pub struct WebhooksQueryBody {
     pub variables: WebhooksVariables,
 }
 
-pub async fn query_webhooks(event: Event) -> Result<String, Error> {
+#[derive(Deserialize, Debug, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Webhook {
+    pub endpoint: String,
+    pub token: Option<String>,
+    pub is_published: bool,
+    pub event_type: EventType,
+}
+
+#[derive(Deserialize, Debug, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct WebhooksResponsePublisher {
+    pub webhooks: Vec<Webhook>,
+}
+
+#[derive(Deserialize, Debug, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct WebhooksResponseImprint {
+    pub publisher: WebhooksResponsePublisher,
+}
+
+#[derive(Deserialize, Debug, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct WebhooksResponseWork {
+    pub imprint: WebhooksResponseImprint,
+}
+
+#[derive(Deserialize, Debug, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct WebhooksResponseData {
+    pub work: WebhooksResponseWork,
+}
+
+#[derive(Deserialize, Debug, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct WebhooksResponseBody {
+    pub data: WebhooksResponseData
+}
+
+pub async fn query_webhooks(event: Event) -> Result<WebhooksResponseBody, Error> {
     let client = reqwest::Client::new();
     let url = "https://api.thoth.pub/graphql".to_string();
     let query =
@@ -58,8 +97,8 @@ query WebhooksQuery($workId: Uuid!, $eventTypes: [EventType!], $isPublished: Boo
         .error_for_status()?;
 
     tracing::info!("response: {:?}", response);
-    let response_text = response.text().await?;
-    tracing::info!("response: {:?}", response_text);
+    let response_text = response.json::<WebhooksResponseBody>().await?;
+    tracing::info!("response_text: {:?}", response_text);
 
     Ok(response_text)
 }
