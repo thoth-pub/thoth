3bdfb6a6d62374ef12d7a295adf3143c11f337d6
rhigman <73792779+rhigman@users.noreply.github.com>
2025-06-18 14:00:12 +0100

    Correct graphql syntax to allow proper json serialisation

--- thoth-processor/src/requests/graphql.rs
+++ thoth-processor/src/requests/graphql.rs
@@ -1,27 +1,40 @@
 use loco_rs::prelude::*;
 use serde::{Deserialize, Serialize};
 use thoth_api::event::model::Event;
+use uuid::Uuid;
+
+#[derive(Deserialize, Debug, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct WebhooksVariables {
+    work_id: Uuid,
+}
 
 #[derive(Deserialize, Debug, Serialize)]
 #[serde(rename_all = "camelCase")]
 pub struct WebhooksQueryBody {
     pub query: String,
+    pub variables: WebhooksVariables,
 }
 
 pub async fn query_webhooks(event: Event) -> Result<String, Error> {
     let client = reqwest::Client::new();
     let url = "https://api.thoth.pub/graphql".to_string();
-    let query = format!(
+    let query =
         "
-{{
-    work(workId: \"{}\") {{
+query WebhooksQuery($workId: Uuid!) {
+    work(workId: $workId) {
         workId
         fullTitle
-    }}
-}}",
-        event.work_id
-    );
-    let body = WebhooksQueryBody { query };
+    }
+}".to_string();
+
+    let variables = WebhooksVariables {
+        work_id: event.work_id,
+    };
+    let body = WebhooksQueryBody {
+        query,
+        variables,
+    };
     let token = "placeholder".to_string();
 
     let response = client
