031a8781d95128e5a4798533913c56e91eee01a2
rhigman <73792779+rhigman@users.noreply.github.com>
2025-07-01 15:06:28 +0100

    Revert "Back loco with Postgres instead of Redis (inc. sqlx issue workaround and hard-coded URL)"
    This reverts commit a7d9fb2c2e15f690ad61c239c560518f8dc106fa.

--- thoth-processor/src/initializers/handle_events.rs
+++ thoth-processor/src/initializers/handle_events.rs
@@ -23,7 +23,10 @@ impl Initializer for HandleEvents {
 
     async fn before_run(&self, ctx: &AppContext) -> Result<()> {
         let ctx = ctx.clone();
-        let redis_url = "redis://localhost:6379";
+        let redis_url = match ctx.config.queue.as_ref().unwrap() {
+            QueueConfig::Redis(queue) => &queue.uri,
+            _ => unreachable!(),
+        };
         let redis = init_pool(redis_url);
 
         tokio::spawn(async move {
