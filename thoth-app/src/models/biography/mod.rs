5e69fa05444a32f5f5d9953506d37e25c165cec5
Javier Arias <javier@jarias.org>
2025-10-30 11:24:16 +0000

    Deprecate thoth-app

--- thoth-app/src/models/biography/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod biographies_by_contribution_query;
-pub mod biography_query;
-pub mod create_biography_mutation;
-pub mod delete_biography_mutation;
-pub mod update_biography_mutation;
--- thoth-app/src/models/title/mod.rs
+++
@@ -1,4 +0,0 @@
-pub mod create_title_mutation;
-pub mod delete_title_mutation;
-pub mod title_query;
-pub mod update_title_mutation;
