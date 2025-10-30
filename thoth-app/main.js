244fa04029b57ccd4ba23325df6411d084c21f1c
Javier Arias <javier@jarias.org>
2025-10-30 11:24:16 +0000

    Deprecate thoth-app

--- thoth-app/main.js
+++
@@ -1,8 +0,0 @@
-import init, { run_app } from "./pkg/thoth_app.js";
-
-async function main() {
-  await init("/admin/thoth_app_bg.wasm");
-  run_app();
-}
-
-main();
--- thoth-app/src/models/abstract/mod.rs
+++
@@ -1,4 +0,0 @@
-pub mod abstract_query;
-pub mod create_abstract_mutation;
-pub mod delete_abstract_mutation;
-pub mod update_abstract_mutation;
