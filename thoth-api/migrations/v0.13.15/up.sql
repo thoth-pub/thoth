26466f76d43ebec83ebfa4eed7ef798f4fa30a63
rhigman <73792779+rhigman@users.noreply.github.com>
2025-11-17 17:00:02 +0000

    Must declare DB constraint as deferrable before it can be set as deferred within transaction

---
+++ thoth-api/migrations/v0.13.15/up.sql
@@ -0,0 +1,3 @@
+ALTER TABLE contribution
+    DROP CONSTRAINT contribution_contribution_ordinal_work_id_uniq,
+    ADD CONSTRAINT contribution_contribution_ordinal_work_id_uniq UNIQUE (contribution_ordinal, work_id) DEFERRABLE INITIALLY IMMEDIATE;
