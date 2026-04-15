f87894bc20d23cfef8591d4cb7add42bbc1c7638
rhigman <73792779+rhigman@users.noreply.github.com>
2026-04-15 16:42:29 +0100

    Add migrations and update changelog

---
+++ thoth-api/migrations/20260415_v1.0.0/down.sql
@@ -0,0 +1,20 @@
+ALTER TABLE file DROP CONSTRAINT IF EXISTS file_type_check;
+ALTER TABLE file_upload DROP CONSTRAINT IF EXISTS file_upload_type_check;
+
+ALTER TABLE file
+  ADD CONSTRAINT file_type_check
+  CHECK (
+    (file_type = 'frontcover' AND work_id IS NOT NULL AND publication_id IS NULL AND additional_resource_id IS NULL AND work_featured_video_id IS NULL) OR
+    (file_type = 'publication' AND publication_id IS NOT NULL AND work_id IS NULL AND additional_resource_id IS NULL AND work_featured_video_id IS NULL) OR
+    (file_type = 'additional_resource' AND additional_resource_id IS NOT NULL AND work_id IS NULL AND publication_id IS NULL AND work_featured_video_id IS NULL) OR
+    (file_type = 'work_featured_video' AND work_featured_video_id IS NOT NULL AND work_id IS NULL AND publication_id IS NULL AND additional_resource_id IS NULL)
+  );
+
+ALTER TABLE file_upload
+  ADD CONSTRAINT file_upload_type_check
+  CHECK (
+    (file_type = 'frontcover' AND work_id IS NOT NULL AND publication_id IS NULL AND additional_resource_id IS NULL AND work_featured_video_id IS NULL) OR
+    (file_type = 'publication' AND publication_id IS NOT NULL AND work_id IS NULL AND additional_resource_id IS NULL AND work_featured_video_id IS NULL) OR
+    (file_type = 'additional_resource' AND additional_resource_id IS NOT NULL AND work_id IS NULL AND publication_id IS NULL AND work_featured_video_id IS NULL) OR
+    (file_type = 'work_featured_video' AND work_featured_video_id IS NOT NULL AND work_id IS NULL AND publication_id IS NULL AND additional_resource_id IS NULL)
+  );
