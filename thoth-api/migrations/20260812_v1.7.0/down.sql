2837686e2b6970c5a2f554c0dd40076731fd71d1
Javier Arias <javier@jarias.org>
2026-08-13 17:19:26 +0100

    Rename migrations

--- thoth-api/migrations/20260812_v1.7.0/down.sql
+++ thoth-api/migrations/20260812_v1.7.0/down.sql
@@ -1,3 +1,6 @@
-DROP TABLE IF EXISTS public.publisher_distribution_platform;
+DROP TABLE IF EXISTS public.publisher_service_configuration_history;
 
-DROP TYPE IF EXISTS public.distribution_platform;
+DROP TYPE IF EXISTS public.publisher_service_configuration_source;
+
+ALTER TABLE public.publisher
+    DROP COLUMN IF EXISTS service_configuration_updated_at;
