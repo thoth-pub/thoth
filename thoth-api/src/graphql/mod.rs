6bb8aa850021857d8a9798b8a6521a4e639c7a05
Javier Arias <javier@jarias.org>
2026-09-22 10:00:26 +0100

    MET-WP7: register reconciliation GraphQL tests

--- thoth-api/src/graphql/mod.rs
+++ thoth-api/src/graphql/mod.rs
@@ -11,6 +11,8 @@ mod metric_dashboard_tests;
 #[cfg(test)]
 mod metric_ingestion_lifecycle_tests;
 #[cfg(test)]
+mod metric_identifier_quarantine_reconciliation_tests;
+#[cfg(test)]
 mod metric_registry_tests;
 #[cfg(test)]
 mod metric_rollup_tests;
